78d37c0b1fea47b9fd301c42438e19eb60d807f5
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-04-21 22:35:52 +0300

    add rout

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
