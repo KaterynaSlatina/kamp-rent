e07673cb72b7e4b0982e07ccf61b3ca11a6bf93d
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-04-25 12:10:42 +0300

    change name

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/kamp_rent">
+        <BrowserRouter basename="/kamp-rent">
           <App />
         </BrowserRouter>
       </PersistGate>
